f55b947bd3e0c0910d6ec0808c1354298ffe39ea
Susen <susen.maharjan@javra.com>
2019-04-23 16:12:15 +0545

    Implementing created nuget package

---
+++ Demostration/CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"103+504 = {Demo.Common.Calculator.Add(103, 504)} \n");
+
+            Console.WriteLine($"546-287 = {Demo.Common.Calculator.Subtract(546, 287)} \n");
+
+            Console.WriteLine($"84*12 = {Demo.Common.Calculator.Multiply(84, 12)} \n");
+
+            Console.WriteLine($"50000/500 = {Demo.Common.Calculator.Divide(50000, 500)} \n");
+        }
+    }
+}
