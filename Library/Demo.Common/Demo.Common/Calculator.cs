a955cb7be3b2843105265f0496dc7e8796682871
Susen <susen.maharjan@javra.com>
2019-04-23 15:27:37 +0545

    Class library project added

---
+++ Library/Demo.Common/Demo.Common/Calculator.cs
@@ -0,0 +1,48 @@
+﻿namespace Demo.Common
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        /// <summary>
+        /// Subtracts two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        public static int Subtract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        /// <summary>
+        /// Multiplies two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        /// <summary>
+        /// Divides two numbers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        public static int Divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
